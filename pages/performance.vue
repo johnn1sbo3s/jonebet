df0b9be4edfce8cc210a3980da2189a580bc1406
johnn1sbo3s <joaopaulosncastro@gmail.com>
2024-08-15 21:08:14 -0300

    Retorna para o fetch normal

--- pages/performance.vue
+++ pages/performance.vue
@@ -278,27 +278,9 @@ const blocksHistoryRows = ref([]);
 const dailyBetsRows = ref([]);
 const monthlyBetsRows = ref([]);
 
-const fetchWithTimeout = (url, options = {}, timeout = 10000) => {
-  return new Promise((resolve, reject) => {
-    const timer = setTimeout(() => {
-      reject(new Error('Tempo de requisição esgotado'));
-    }, timeout);
-
-    fetch(url, options)
-      .then(response => {
-        clearTimeout(timer);
-        resolve(response);
-      })
-      .catch(err => {
-        clearTimeout(timer);
-        reject(err);
-      });
-  });
-};
-
 const fetchData = async (url) => {
   try {
-    const req = await fetchWithTimeout(url, {}, 20000); // timeout de 20 segundos
+    const req = await fetch(url);
     const data = await req.json();
     return data;
   } catch (error) {
