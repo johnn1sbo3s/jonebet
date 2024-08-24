fce5a4290ae0f0991019d90891c03dff01f99ac1
john1sbo3s <joaopaulosncastro@gmail.com>
2024-08-24 17:16:45 -0300

    Ajusta o hover dos cards

--- components/batchCard.vue
+++ components/batchCard.vue
@@ -1,16 +1,19 @@
 <template>
-    <u-card class="max-w-[600px] hover:cursor-pointer hover:outline hover:outline-violet-400 hover:outline-1 group">
+    <u-card
+        class="max-w-[600px] hover:cursor-pointer hover:outline hover:outline-violet-400 hover:outline-1 group"
+        @click="emit('click')"
+    >
         <template #header>
             <div class="flex justify-between align-baseline">
-                <div class="font-semibold flex gap-5 group-hover:text-violet-700">
+                <div class="font-semibold flex gap-5">
                     <p>{{ metricItem.modelo }}</p>
                     <div class="flex gap-3">
-                        <p :class="metricItem.profit > 0 ? 'text-green-600' : 'text-red-600'" class="group-hover:text-violet-700">{{ metricItem.profit > 0 ? '+' : '' }}{{ metricItem.profit }} u</p>
+                        <p :class="metricItem.profit > 0 ? 'text-green-600' : 'text-red-600'">{{ metricItem.profit > 0 ? '+' : '' }}{{ metricItem.profit }} u</p>
                         <u-divider orientation="vertical" class="w-min"/>
                         <p>{{ metricItem.qtd_jgs }} jogos</p>
                     </div>
                 </div>
-                <i class="i-heroicons-chevron-right text-2xl group-hover:text-violet-700"></i>
+                <i class="i-heroicons-chevron-right text-2xl text-violet-500 hidden group-hover:block"></i>
             </div>
         </template>
 
@@ -44,14 +47,19 @@
 <script setup>
 import { DateTime } from 'luxon';
 
+// Props
 const props = defineProps({
     metricItem: {
         type: Object,
         required: true,
         default: () => {}
-    }
+    },
 });
 
+// Emits
+const emit = defineEmits(['click']);
+
+// Variáveis computadas
 const blockDays = computed(() => {
     let lastDay = DateTime.fromISO(props.metricItem.last_block_day);
     return Math.floor(DateTime.now().diff(lastDay, 'days').toObject().days);
