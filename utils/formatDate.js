097891ba108ec3f44055e5c7683cce53214b022b
john1sbo3s <joaopaulosncastro@gmail.com>
2024-08-31 13:27:36 -0300

    Refatora util de formatar data para formato br

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,4 +1,8 @@
-export function formatDate(date) {
-    const [year, month, day] = date.split('-');
+export function formatDate(dateString) {
+    const date = new Date(Date.parse(dateString));
+    const day = `0${date.getDate()}`.slice(-2);
+    const month = `0${date.getMonth() + 1}`.slice(-2);
+    const year = date.getFullYear();
+
     return `${day}/${month}/${year}`;
 }
