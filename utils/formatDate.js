498d193e3b45273552fc2a28a607d833ca9a2ab1
john1sbo3s <joaopaulosncastro@gmail.com>
2024-09-03 15:52:06 -0300

    Corrige util de formatar datas

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,8 +1,4 @@
-export function formatDate(dateString) {
-    const date = new Date(Date.parse(dateString));
-    const day = `0${date.getDate()}`.slice(-2);
-    const month = `0${date.getMonth() + 1}`.slice(-2);
-    const year = date.getFullYear();
-
+export function formatDate(date) {
+    const [year, month, day] = date.split('-');
     return `${day}/${month}/${year}`;
 }
