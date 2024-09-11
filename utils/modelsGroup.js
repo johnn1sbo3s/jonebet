22fa9b95407c66af833fcbf1eb4e48f1f4af64f2
john1sbo3s <joaopaulosncastro@gmail.com>
2024-09-11 08:24:27 -0300

    Ajusta modelos da lista de escolhidos

--- utils/modelsGroup.js
+++ utils/modelsGroup.js
@@ -6,12 +6,9 @@ export const FAVORITE_MODELS = [
 
 export const CHOSEN_MODELS = [
         "lay_0x2_v5", "lay_0x2_v6", "lay_0x2_v7",
-        "lay_2x0_v2", "back_draw_v4", "lay_home_v11",
+        "lay_2x0_v2", "back_draw_v4",
         "back_home_v3", "ltd_v1_betfair", "lay_2x0_v1_blend",
-        "lay_2x0_v1_svm", "lay_2x0_v5", "lay_home_v10",
-        "lay_home_v8", "lay_home_v9","lay_home_v10_betfair",
-        "lay_home_v11_betfair", "lay_home_v8_betfair", "lay_home_v9_betfair",
-        "lay_away_v9", "lay_goleada_home_v1",
-        "lay_goleada_home_v3", "lay_0x1", "back_home_v2"
+        "lay_2x0_v1_svm", "lay_2x0_v5", "lay_home_v8", "lay_home_v10_betfair",
+        "lay_goleada_home_v1", "lay_goleada_home_v3", "lay_0x1", "back_home_v2",
+        "lay_goleada_home_v1_betfair"
 ]
-
