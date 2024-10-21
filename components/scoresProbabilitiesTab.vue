083067c2688796b7afa8394748f1b8a0d8b532ce
john1sbo3s <joaopaulosncastro@gmail.com>
2024-10-21 20:55:51 -0300

    Adiciona informação de jogo ao vivo

--- components/scoresProbabilitiesTab.vue
+++ components/scoresProbabilitiesTab.vue
@@ -1,11 +1,30 @@
 <template>
-	<USelectMenu
-		class="w-1/5 mb-4"
-		v-model="selectedOption"
-		:options="options"
-		searchable
-		option-attribute="label"
-	/>
+	<div class="flex gap-5 items-baseline">
+		<USelectMenu
+			class="w-1/4 mb-4"
+			v-model="selectedOption"
+			:options="options"
+			searchable
+		>
+			<template #option="{ option }">
+				<div class="flex gap-2 items-center my-1">
+					<span>
+						{{ option.label }}
+					</span>
+					<span class="text-xs text-green-500 font-semibold mt-0.5">
+						{{ isGameLive(option) ? 'AO VIVO' : '' }}
+					</span>
+				</div>
+			</template>
+		</USelectMenu>
+
+		<div
+			v-if="isGameLive(selectedOption)"
+			class="text-green-500 font-semibold"
+		>
+			JOGO AO VIVO
+		</div>
+	</div>
 
 	<div class="grid gap-3 grid-cols-2">
 		<div class="flex gap-3">
@@ -38,6 +57,7 @@ const props = defineProps({
 
 const selectedOption = ref(null);
 const options = ref([]);
+const timeNow = ref(null);
 
 onMounted(() => {
 	options.value = props.data.filter(item => item.Date === new Date().toISOString().split('T')[0]).map((item) => ({
@@ -46,10 +66,25 @@ onMounted(() => {
 		value: item._id,
 	}));
 	selectedOption.value = options.value[0];
+	timeNow.value = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 });
+
+import { DateTime } from 'luxon';
+
+function isGameLive(game) {
+	if (!game) {
+		return false;
+	}
+	let now = DateTime.now();
+	let gameTime = DateTime.fromFormat(game.Time, 'HH:mm');
+	let diffInMinutes = now.diff(gameTime, 'minutes').minutes;
+
+	return diffInMinutes >= 0 && diffInMinutes <= 120;
+}
+
 
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 
 </style>
