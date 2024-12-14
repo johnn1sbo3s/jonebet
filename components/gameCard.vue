589d64893f623277515962bfad4ab87708d6263c
john1sbo3s <joaopaulosncastro@gmail.com>
2024-12-14 20:39:42 -0300

    Componetiza o card de jogo

---
+++ components/gameCard.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="flex flex-col">
+        <div
+            v-for="item in data"
+            :key="item._id"
+            class="flex gap-7 justify-between items-center w-full border border-gray-200 rounded-lg py-4 px-6 mb-2"
+            :class="clicky ? 'cursor-pointer hover:outline hover:outline-violet-400 hover:outline-1' : ''"
+            @click="clicky ? emits('click', item) : ''"
+        >
+            <div class="flex gap-7 items-center">
+                <div
+                    class="flex flex-col items-center"
+                >
+                    <div class="font-semibold">
+                        {{ item.Time }}
+                    </div>
+
+                    <div class="text-sm text-gray-700">
+                        {{ formatDate(item.Date) }}
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-2">
+                    <div>
+                        {{ item.Home }} x {{ item.Away }}
+                    </div>
+
+                    <div class="flex gap-1">
+                        <UBadge color="primary" variant="soft">{{ item.FT_Odds_H.toFixed(2) }}</UBadge>
+                        <UBadge color="primary" variant="soft">{{ item.FT_Odds_D.toFixed(2) }}</UBadge>
+                        <UBadge color="primary" variant="soft">{{ item.FT_Odds_A.toFixed(2) }}</UBadge>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex flex-col items-end gap-1">
+                <div class="text-xs text-gray-500">
+                    Modelo
+                </div>
+
+                <div class="text-gray-600 text-sm font-semibold">
+                    {{ modelNameToNaturalName(item.Modelo) }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    data: {
+        type: Array,
+        required: true
+    },
+
+    clicky: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const emits = defineEmits(['click']);
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- components/twoGoalsTab.vue
+++ components/twoGoalsTab.vue
@@ -16,47 +16,28 @@
             />
         </div>
 
-        <div
-            v-for="item in filteredGames"
-            :key="item._id"
-            class="flex gap-7 justify-between items-center w-full border border-gray-200 rounded-lg py-4 px-6 mb-2"
-        >
-            <div class="flex gap-7 items-center">
-                <div
-                    class="flex flex-col items-center"
-                >
-                    <div class="font-semibold">
-                        {{ item.Time }}
-                    </div>
-
-                    <div class="text-sm text-gray-700">
-                        {{ formatDate(item.Date) }}
-                    </div>
-                </div>
-
-                <div class="flex flex-col gap-2">
-                    <div>
-                        {{ item.Home }} x {{ item.Away }}
-                    </div>
-
-                    <div class="flex gap-1">
-                        <UBadge color="primary" variant="soft">{{ item.FT_Odds_H.toFixed(2) }}</UBadge>
-                        <UBadge color="primary" variant="soft">{{ item.FT_Odds_D.toFixed(2) }}</UBadge>
-                        <UBadge color="primary" variant="soft">{{ item.FT_Odds_A.toFixed(2) }}</UBadge>
-                    </div>
-                </div>
+        <div class="flex gap-3">
+            <div class="w-1/2">
+                <game-card
+                    class="w-full"
+                    :data="filteredGames"
+                    clicky
+                />
             </div>
 
-            <div class="flex flex-col items-end gap-1">
-                <div class="text-xs text-gray-500">
-                    Modelo
-                </div>
-
-                <div class="text-gray-600 text-sm font-semibold">
-                    {{ modelNameToNaturalName(item.Modelo) }}
+            <div
+                class="w-1/2 h-full sticky top-4"
+            >
+                <div
+                    class="w-full h-[50svh] flex items-center justify-center outline-dashed outline-1 outline-gray-400 p-10 rounded-md"
+                >
+                    <p class="text-center text-gray-400 text-2xl">
+                        Selecione um card ao lado para ver o gráfico de acúmulo de capital do modelo.
+                    </p>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
@@ -78,6 +59,7 @@ const acceptedModels = [
 
 const chosenDay = ref(null);
 const chosenModel = ref({ label: 'Todos os modelos', value: null });
+const chosenGame = ref(null);
 
 const filteredGames = computed(() => {
     let filtered = props.data.filter((item) => acceptedModels.includes(item.Modelo));
