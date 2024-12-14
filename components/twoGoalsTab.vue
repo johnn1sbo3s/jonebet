5f8156682ed63a2d486ca5d14e80d0be21e05566
john1sbo3s <joaopaulosncastro@gmail.com>
2024-12-14 01:09:56 -0300

    Implementa filtros de data e modelo

--- components/twoGoalsTab.vue
+++ components/twoGoalsTab.vue
@@ -1,42 +1,60 @@
 <template>
-    <div
-        v-for="item in filteresGames"
-        :key="item._id"
-        class="flex gap-7 justify-between items-center w-full border border-gray-200 rounded-lg py-4 px-6 mb-2"
-    >
-        <div class="flex gap-7 items-center">
-            <div
-                class="flex flex-col items-center"
-            >
-                <div class="font-semibold">
-                    {{ item.Time }}
-                </div>
+    <div>
+        <div class="flex gap-2 mb-4">
+            <USelectMenu
+                class="w-1/5"
+                placeholder="Selecione um dia"
+                :options="datesOptions"
+                v-model="chosenDay"
+            />
 
-                <div class="text-sm text-gray-700">
-                    {{ formatDate(item.Date) }}
-                </div>
-            </div>
-
-            <div class="flex flex-col gap-2">
-                <div>
-                    {{ item.Home }} x {{ item.Away }}
-                </div>
-
-                <div class="flex gap-1">
-                    <UBadge color="primary" variant="soft">{{ item.FT_Odds_H.toFixed(2) }}</UBadge>
-                    <UBadge color="primary" variant="soft">{{ item.FT_Odds_D.toFixed(2) }}</UBadge>
-                    <UBadge color="primary" variant="soft">{{ item.FT_Odds_A.toFixed(2) }}</UBadge>
-                </div>
-            </div>
+            <USelectMenu
+                class="w-1/5"
+                placeholder="Selecione uma opção"
+                :options="modelsOptions"
+                v-model="chosenModel"
+            />
         </div>
 
-        <div class="flex flex-col items-end gap-1">
-            <div class="text-xs text-gray-500">
-                Modelo
+        <div
+            v-for="item in filteredGames"
+            :key="item._id"
+            class="flex gap-7 justify-between items-center w-full border border-gray-200 rounded-lg py-4 px-6 mb-2"
+        >
+            <div class="flex gap-7 items-center">
+                <div
+                    class="flex flex-col items-center"
+                >
+                    <div class="font-semibold">
+                        {{ item.Time }}
+                    </div>
+
+                    <div class="text-sm text-gray-700">
+                        {{ formatDate(item.Date) }}
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-2">
+                    <div>
+                        {{ item.Home }} x {{ item.Away }}
+                    </div>
+
+                    <div class="flex gap-1">
+                        <UBadge color="primary" variant="soft">{{ item.FT_Odds_H.toFixed(2) }}</UBadge>
+                        <UBadge color="primary" variant="soft">{{ item.FT_Odds_D.toFixed(2) }}</UBadge>
+                        <UBadge color="primary" variant="soft">{{ item.FT_Odds_A.toFixed(2) }}</UBadge>
+                    </div>
+                </div>
             </div>
 
-            <div class="text-gray-600 text-sm font-semibold">
-                {{ modelNameToNaturalName(item.Modelo) }}
+            <div class="flex flex-col items-end gap-1">
+                <div class="text-xs text-gray-500">
+                    Modelo
+                </div>
+
+                <div class="text-gray-600 text-sm font-semibold">
+                    {{ modelNameToNaturalName(item.Modelo) }}
+                </div>
             </div>
         </div>
     </div>
@@ -55,12 +73,42 @@ const acceptedModels = [
     'lay_2_goals_home',
     'lay_2_goals_away',
     'lay_away_v1_cluster',
-    'lay_away_v3_cluster',
-    'lay_away_v13_betfair',
+    'lay_away_v3_cluster'
 ]
 
-const filteresGames = computed(() => {
-    return props.data.filter((item) => acceptedModels.includes(item.Modelo));
+const chosenDay = ref(null);
+const chosenModel = ref({ label: 'Todos os modelos', value: null });
+
+const filteredGames = computed(() => {
+    let filtered = props.data.filter((item) => acceptedModels.includes(item.Modelo));
+    filtered = filtered.filter((item) => formatDate(item.Date) === chosenDay.value);
+
+    if (chosenModel.value.value) {
+        filtered = filtered.filter((item) => item.Modelo === chosenModel.value.value);
+    }
+
+    return filtered;
+});
+
+const datesOptions = computed(() => {
+	let dates = [...new Set(Object.values(props.data).map(item => formatDate(item.Date)))];
+	dates = dates.slice(-7);
+
+	return dates;
+});
+
+const modelsOptions = computed(() => {
+    return [
+        { label: 'Todos os modelos', value: null },
+        ...acceptedModels.map((item) => ({
+            label: modelNameToNaturalName(item),
+            value: item
+        }))
+    ]
+});
+
+onMounted(() => {
+    chosenDay.value = datesOptions.value.at(-1);
 });
 
 </script>
