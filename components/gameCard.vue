41a612ad9064131ea418ffad8f9745bcef0a1436
john1sbo3s <joaopaulosncastro@gmail.com>
2025-01-10 22:21:58 -0300

    Corrige abas de alavancagem

--- components/gameCard.vue
+++ components/gameCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col">
         <div
-            v-for="item in internalFixtures"
+            v-for="item in data"
             :key="item._id"
             class="flex gap-7 justify-between items-center w-full border border-gray-200 rounded-lg py-4 px-6 mb-2 cursor-pointer hover:outline hover:outline-violet-400 hover:outline-1"
             :class="item._id === chosen._id ? 'outline outline-violet-400 outline-1' : ''"
@@ -35,21 +35,11 @@
 
             <div class="flex flex-col items-end gap-1">
                 <div class="text-xs text-gray-500">
-                    Entrada em
+                    Modelo
                 </div>
 
-                <div
-                    v-if="countModels(item) === 0"
-                    class="text-gray-600 text-sm font-semibold"
-                >
-                    Nenhum modelo
-                </div>
-
-                <div
-                    v-else
-                    class="text-gray-600 text-sm font-semibold"
-                >
-                    {{ countModels(item) }} {{ countModels(item) === 1 ? 'modelo' : 'modelos' }}
+                <div class="text-gray-600 text-sm font-semibold">
+                    {{ modelNameToNaturalName(item.Modelo) }}
                 </div>
             </div>
         </div>
@@ -59,31 +49,23 @@
 <script setup>
 
 const props = defineProps({
-    fixtures: {
+    data: {
         type: Array,
         required: true
     },
-    bets: {
-        type: Array,
-        required: true
+
+    clicky: {
+        type: Boolean,
+        default: false
     },
+
     chosen: {
         type: Object,
         default: () => {}
-    },
+    }
 });
 
 const emits = defineEmits(['click']);
-
-const internalFixtures = ref([]);
-
-watch(() => props.fixtures, (value) => {
-    internalFixtures.value = value;
-}, { immediate: true });
-
-function countModels(fixture) {
-    return props.bets.filter((bet) => bet.Date === fixture.Date && bet.Home === fixture.Home && bet.Away === fixture.Away).length;
-}
 
 </script>
 
