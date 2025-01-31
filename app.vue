6965a41de25b1841f0fc8475d78e48a5e1604325
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-01-31 15:06:19 -0300

    Remove código do mouseflow do app.vue

--- app.vue
+++ app.vue
@@ -77,16 +77,6 @@ const links = [
 		},
 	],
 ];
-
-onMounted(() => {
-	window._mfq = window._mfq || [];
-	(function() {
-		var mf = document.createElement("script");
-		mf.type = "text/javascript"; mf.defer = true;
-		mf.src = "//cdn.mouseflow.com/projects/5385053f-80d7-4871-bb21-d736a7e91ba4.js";
-		document.getElementsByTagName("head")[0].appendChild(mf);
-	})();
-})
 </script>
 
 <style>
