2adcee935cdbbac748eb41e68e3f2cba8ba00dc3
john1sbo3s <joaopaulosncastro@gmail.com>
2025-02-01 15:39:40 -0300

    Add script do mouseflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,23 +3,32 @@ export default defineNuxtConfig({
   modules: ["@nuxt/ui", "nuxt-lodash", "@nuxt/fonts", "@pinia/nuxt"],
   devtools: { enabled: false },
 
-  compatibilityDate: '2024-08-15',
+  app: {
+	head: {
+	  script: [
+		{
+		  src: "//cdn.mouseflow.com/projects/5385053f-80d7-4871-bb21-d73a67e291da.js",
+		  defer: true,
+		},
+	  ],
+	},
+  },
 
   lodash: {
-    prefix: "_",
-    prefixSkip: ["string"],
-    upperAfterPrefix: false,
-    exclude: [],
-    alias: [
-      ["camelCase", "stringToCamelCase"], // => stringToCamelCase
-      ["kebabCase", "stringToKebab"], // => stringToKebab
-      ["isDate", "isLodashDate"], // => _isLodashDate
-    ],
+	prefix: "_",
+	prefixSkip: ["string"],
+	upperAfterPrefix: false,
+	exclude: [],
+	alias: [
+		["camelCase", "stringToCamelCase"], // => stringToCamelCase
+		["kebabCase", "stringToKebab"], // => stringToKebab
+		["isDate", "isLodashDate"], // => _isLodashDate
+	],
   },
 
   runtimeConfig: {
-    public: {
-      API_URL: 'https://api.jonebet.xyz',
-    },
+	public: {
+		API_URL: 'https://api.jonebet.xyz',
+	},
   }
 });
