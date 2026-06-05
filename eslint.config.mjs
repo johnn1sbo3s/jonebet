d73eb8d6b2437f661754fbf091876bbc457b537e
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-06-05 10:14:00 -0300

    chore: add custom ESLint rules (blank line siblings) and Tailwind check script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,28 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config'
+import blankLineBetweenSiblings from './eslint-rules/vue-blank-line-between-siblings.js'
 
-export default createConfigForNuxt({})
+const customPlugin = {
+  files: ['**/*.vue'],
+  plugins: {
+    custom: {
+      rules: {
+        'vue-blank-line-between-siblings': blankLineBetweenSiblings
+      }
+    }
+  },
+  rules: {
+    'custom/vue-blank-line-between-siblings': 'error'
+  }
+}
+
+export default createConfigForNuxt(
+  { ignores: ['scripts/**/*.cjs'] },
+  customPlugin,
+  {
+    rules: {
+      'vue/no-multiple-template-root': 'off',
+      'vue/max-attributes-per-line': ['error', { singleline: 3 }],
+      'nuxt/nuxt-config-keys-order': 'off'
+    }
+  }
+)
