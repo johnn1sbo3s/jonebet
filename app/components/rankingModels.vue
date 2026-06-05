e386a55d6c2a9331b3dd1dc4989568a66f63201c
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-06-05 10:50:40 -0300

    Remove default redundante das props

--- app/components/rankingModels.vue
+++ app/components/rankingModels.vue
@@ -93,17 +93,14 @@ const props = defineProps({
     items: {
         type: Array,
         required: true,
-        default: () => []
     },
     title: {
         type: String,
         required: true,
-        default: () => ""
     },
     allResultsData: {
         type: Array,
         required: true,
-        default: () => []
     }
 })
 
