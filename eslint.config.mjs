442b19ce5504425f089470d3b8b5dde662f987ff
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-06-05 21:56:41 -0300

    chore: add vscode settings and eslint indent rule for 2-space indentation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -83,7 +83,8 @@ export default createConfigForNuxt(
       'vue/no-multiple-template-root': 'off',
       'vue/max-attributes-per-line': ['error', { singleline: 3 }],
       'vue/component-name-in-template-casing': ['error', 'PascalCase', { registeredComponentsOnly: false }],
-      'nuxt/nuxt-config-keys-order': 'off'
+      'nuxt/nuxt-config-keys-order': 'off',
+      'indent': ['error', 2]
     }
   }
 )
