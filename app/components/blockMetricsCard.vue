8803edc2813d104666cca62ecd7039176ca0cc9b
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-06-13 00:32:41 -0300

    style(front): apply zinc-950/80 to inner cards inside block-metrics

--- app/components/blockMetricsCard.vue
+++ app/components/blockMetricsCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <UCard class="h-full">
+  <UCard class="h-full border border-zinc-800 bg-zinc-950/80">
     <template #header>
       <p class="font-semibold">{{ cardTitle }}</p>
     </template>
