f3e237a8d74b479993b87993f7c09892dff9d782
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-08-08 20:27:27 -0300

    style(scanner): h-102 no skeleton em vez de arbitrary value h-[25.5rem]

--- app/components/scannerSkeleton.vue
+++ app/components/scannerSkeleton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-1 gap-3 md:grid-cols-2 xl:grid-cols-3">
-    <USkeleton v-for="i in 6" :key="i" class="h-[25.5rem] rounded-2xl" />
+    <USkeleton v-for="i in 6" :key="i" class="h-102 rounded-2xl" />
   </div>
 </template>
 
